52420330894f42a12725ad04c24029ba03bd9e5a
Dilwoar Hussain <DilwoarH@users.noreply.github.com>
2020-05-23 19:56:26 +0100

    Update freshchat.js

--- js/freshchat.js
+++ js/freshchat.js
@@ -1,3 +1,7 @@
+/*
+
+DISABLED CHAT - ONLY ABLE FOR RAMADAN
+
 function initFreshChat() {
   window.fcWidget.init({
     token: "6341f56b-59da-479f-81d7-e70c9a220f5e",
@@ -6,4 +10,5 @@ function initFreshChat() {
 }
 function initialize(i,t){var e;i.getElementById(t)?initFreshChat():((e=i.createElement("script")).id=t,e.async=!0,e.src="https://wchat.eu.freshchat.com/js/widget.js",e.onload=initFreshChat,i.head.appendChild(e))}function initiateCall(){initialize(document,"freshchat-js-sdk")}window.addEventListener?window.addEventListener("load",initiateCall,!1):window.attachEvent("load",initiateCall,!1);
 
+*/
 
